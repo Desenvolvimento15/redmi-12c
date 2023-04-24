f0c7cc10a3b685868d4f9d3e750ca6f328b9f86e
desenvolvimento15 <desenvolvimento15@dl.com.br>
2023-04-24 09:54:27 -0300

    vs01

---
+++ desk/assets/js/main.js
@@ -0,0 +1,12 @@
+$(function (){
+    $('.galeria').slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots: true,
+    autoplay: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+              });
+        })
